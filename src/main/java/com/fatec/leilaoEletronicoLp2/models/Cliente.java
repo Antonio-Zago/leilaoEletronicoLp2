6a779264f80bb441fa3e0612e042637ae31529e6
Daniloel <zoltancao@gmail.com>
2023-10-07 23:29:10 -0300

    Adição do lombok

--- src/main/java/com/fatec/leilaoEletronicoLp2/models/Cliente.java
+++ src/main/java/com/fatec/leilaoEletronicoLp2/models/Cliente.java
@@ -3,45 +3,22 @@ package com.fatec.leilaoEletronicoLp2.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 @Table(name = "CLIENTE")
 public class Cliente {
 	
 	@Id
 	private String cliCpf;
-	
-	
 	private String cliNome;
 	private String cliEmail;
-	
-	public Cliente() {}
-	
-	
-	public Cliente(String cliCpf, String cliNome, String cliEmail) {
-		super();
-		this.cliCpf = cliCpf;
-		this.cliNome = cliNome;
-		this.cliEmail = cliEmail;
-	}
-	public String getCliCpf() {
-		return cliCpf;
-	}
-	public void setCliCpf(String cliCpf) {
-		this.cliCpf = cliCpf;
-	}
-	public String getCliNome() {
-		return cliNome;
-	}
-	public void setCliNome(String cliNome) {
-		this.cliNome = cliNome;
-	}
-	public String getCliEmail() {
-		return cliEmail;
-	}
-	public void setCliEmail(String cliEmail) {
-		this.cliEmail = cliEmail;
-	}
-	
-	
+
 }
